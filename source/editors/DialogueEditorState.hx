5b1b81270eb3a20efdc0c18a9a95f3aa2cbf13cc
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-08-03 19:33:09 -0400

    forgot the editor state

--- source/editors/DialogueEditorState.hx
+++ source/editors/DialogueEditorState.hx
@@ -70,7 +70,9 @@ class DialogueEditorState extends MusicBeatState
 		add(character);
 
 		box = new FlxSprite(70, 370);
-		box.frames = Paths.getSparrowAtlas('speech_bubble');
+		var tex_str = "speech_bubble";
+		if (ClientPrefs.data.darkmode) tex_str = "speech_bubble_dark";
+		box.frames = Paths.getSparrowAtlas(tex_str);
 		box.scrollFactor.set();
 		box.antialiasing = ClientPrefs.data.antialiasing;
 		box.animation.addByPrefix('normal', 'speech bubble normal', 24);
